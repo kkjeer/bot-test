eaaeea92a7f492ee7eb8e98ff93f6a171b7e197a
kkjeer <kkjeer@wellesley.edu>
2019-05-10 13:44:25 -0700

    Update automergeAdvancedSettings.js

--- automergeAdvancedSettings.js
+++ automergeAdvancedSettings.js
@@ -2,3 +2,4 @@ console.log("testing AutoMerge advanced settings");
 console.log("new changes");
 console.log("new changes from local");
 console.log("editing online in GitHub");
+console.log("edit by kkjeer");
